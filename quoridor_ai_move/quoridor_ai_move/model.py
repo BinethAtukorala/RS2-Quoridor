c4f6ab529b9f175c21d1d663d793ce6e222ff8cb
Bihan Sudusinghe <b.y.sudusinghe@gmail.com>
2026-04-27 11:20:40 +1000

    Removed unnecessary comments for clarity

--- quoridor_ai_move/quoridor_ai_move/model.py
+++ quoridor_ai_move/quoridor_ai_move/model.py
@@ -12,37 +12,6 @@ try:
 except ImportError:
     from encoder import BOARD_N, NUM_ACTIONS, STATE_CHANNELS
 
-
-# def _residual_block(x, filters: int):
-#     # Standard pre-activation residual block: two 3x3 convs with batch norm,
-#     # added back to the input so gradients flow through deeper stacks.
-#     shortcut = x
-#     x = layers.Conv2D(filters, 3, padding="same", use_bias=False)(x)
-#     x = layers.BatchNormalization()(x)
-#     x = layers.ReLU()(x)
-#     x = layers.Conv2D(filters, 3, padding="same", use_bias=False)(x)
-#     x = layers.BatchNormalization()(x)
-#     x = layers.Add()([x, shortcut])
-#     x = layers.ReLU()(x)
-#     return x
-
-# def build_qnet(filters: int = 64, n_blocks: int = 4) -> Model:
-#     # Input: encoded board tensor from encoder.encode_state.
-#     inp = layers.Input(shape=(BOARD_N, BOARD_N, STATE_CHANNELS), name="state")
-#     # Stem conv brings the input up to `filters` channels before the resnet.
-#     x = layers.Conv2D(filters, 3, padding="same", use_bias=False)(inp)
-#     x = layers.BatchNormalization()(x)
-#     x = layers.ReLU()(x)
-#     # Residual trunk -- depth/width controlled by callers.
-#     for _ in range(n_blocks):
-#         x = _residual_block(x, filters)
-#     # Dense head produces one scalar Q-value for each of the NUM_ACTIONS slots.
-#     x = layers.Flatten()(x)
-#     x = layers.Dense(256, activation="relu")(x)
-#     q = layers.Dense(NUM_ACTIONS, activation=None, name="q")(x)
-#     return Model(inp, q, name="quoridor_qnet")
-
-
 def build_qnet(filters: int = 64, n_blocks: int = 4, dropout: float = 0.1) -> Model:
     reg = tf.keras.regularizers.l2(1e-4)
     inp = layers.Input(shape=(BOARD_N, BOARD_N, STATE_CHANNELS), name="state")
